c0ea6a2b10c248baa1d3e1e34b730aa8693a1014
Jyotirmayee Parida <81229488+jyotirmayee2312@users.noreply.github.com>
2023-08-21 15:18:47 +0530

    Update ssl_expiry_check.py

--- ssl_expiry_check.py
+++ ssl_expiry_check.py
@@ -1,6 +1,9 @@
 import json
 import requests
 import os
+from datetime import datetime
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
 
 def send_slack_notification(domain, days_to_expire):
     message = f"SSL Expiry Alert\n   * Domain: {domain}\n   * Warning: The SSL certificate for {domain} will expire in {days_to_expire} days."
@@ -15,6 +18,13 @@ def send_slack_notification(domain, days_to_expire):
     except requests.exceptions.RequestException as e:
         print("Error sending Slack notification:", e)
 
+def calculate_days_until_expiry(cert_data):
+    cert = x509.load_pem_x509_certificate(cert_data, default_backend())
+    expiration_date = cert.not_valid_after
+    current_date = datetime.utcnow()
+    days_until_expiry = (expiration_date - current_date).days
+    return days_until_expiry
+
 def main():
     try:
         with open("domains.json", "r") as file:
@@ -22,14 +32,18 @@ def main():
             domains = domains_data["domains"]
 
             for domain in domains:
-                # Here you can implement the logic to check SSL expiry for each domain and calculate days_to_expire
-                days_to_expire = 30  # Replace with your actual calculation
+                try:
+                    # Retrieve SSL certificate for the domain and calculate days_until_expiry
+                    response = requests.get(f"https://{domain}")
+                    cert_data = response.content
+                    days_to_expire = calculate_days_until_expiry(cert_data)
 
-                if days_to_expire <= 30:
-                    send_slack_notification(domain, days_to_expire)
+                    if days_to_expire <= 30:
+                        send_slack_notification(domain, days_to_expire)
+                except requests.exceptions.RequestException as e:
+                    print(f"Error fetching SSL certificate for {domain}:", e)
     except (FileNotFoundError, json.JSONDecodeError) as e:
         print("Error loading or processing domains:", e)
 
 if __name__ == "__main__":
     main()
-
